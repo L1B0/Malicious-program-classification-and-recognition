33e46298820cb9fb3d21cd1f7cd825859889e38f
L1B0 <798151029@qq.com>
2019-09-07 16:24:22 +0800

    Update

---
+++ TFIDF/tfidf.py
@@ -0,0 +1,20 @@
+# coding=UTF-8
+#!/usr/bin/python3
+
+import os
+from TFIDF import tfidf
+from TFIDF import getApiList
+from TFIDF import calcTF
+from tqdm import tqdm
+
+def start(ida_path,now_pwd):
+
+	getApiList.start(ida_path,now_pwd)
+	
+	api_dic = '/'.join(now_pwd.split('/')[:-1]) + '/api'
+	if os.path.exists(api_dic) == False:
+		os.makedirs(api_dic)
+	func_dic = '/'.join(now_pwd.split('/')[:-1]) + '/functimes'
+	if os.path.exists(func_dic) == False:
+		os.makedirs(func_dic)
+	calcTF.start(api_dic)
